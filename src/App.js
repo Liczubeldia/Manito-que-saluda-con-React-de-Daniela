b86325f9a2b8cca8585b9183d98e1c6f80d663af
Daniela Zubeldía <liczubeldia@gmail.com>
2024-08-29 10:39:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WavingHand from "./WavingHand";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>¡Hola!, soy Daniela </h1>
+      <h3>Estoy aprendiendo Front End</h3>
+      <WavingHand />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WavingHand.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const WavingHand = () => {
+  return (
+    <div className="hand-container">
+      <span className="hand">👋</span>
+    </div>
+  );
+};
+
+export default WavingHand;
